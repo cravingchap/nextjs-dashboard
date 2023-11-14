21964eede4cad2f049445c097141a2609e7717c2
Siddharth Singh <siddharthsingh7103@gmail.com>
2023-11-15 00:21:07 +0530

    first

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={'${inter.className} antialiased'}>{children}</body>
+    </html>
+  );
+}
+
+
+
+// the layout file is the best way to create a shared layout that all pages in your application can use.
+
